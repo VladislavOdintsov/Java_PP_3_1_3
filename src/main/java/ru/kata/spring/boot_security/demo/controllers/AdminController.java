f3c544f6b762908acf015fa1a98d6e812754724d
VladislavOdintsov <youngmonkey@yandex.ru>
2022-12-08 19:40:03 +0300

    Commit_3

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -1,7 +1,5 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -9,46 +7,42 @@ import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.services.UserService;
 
 @Controller
-public class AppController {
+@RequestMapping("/admin")
+public class AdminController {
 
     private final UserService userService;
 
-    public AppController(UserService userService) {
+    public AdminController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/user")
-    public String user(Model model) {
-        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        model.addAttribute("user", user);
-        return "user";
-    }
 
 
-    @GetMapping(value = "/admin")
+
+    @GetMapping
     public String tableUsers(Model model) {
         model.addAttribute("users", userService.allUsers());
         return "admin";
     }
 
-    @GetMapping("/admin/addUser")
+    @GetMapping("/addUser")
     public String newUser(@ModelAttribute("user") User user) {
         return "/addUser";
     }
 
-    @PostMapping("/admin/addUser")
+    @PostMapping("/addUser")
     public String addUser(@ModelAttribute("user") User user) {
         userService.addUser(user);
         return "redirect:/admin";
     }
 
-    @GetMapping("/admin/changeUser/{id}")
+    @GetMapping("/changeUser/{id}")
     public String changeUser(Model model, @PathVariable("id") long id) {
         model.addAttribute("user", userService.getUserById(id));
         return "/changeUser";
     }
 
-    @PatchMapping("/admin/changeUser/{id}")
+    @PatchMapping("/changeUser/{id}")
     public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") long id) {
         userService.changeUser(id, user);
         return "redirect:/admin";
